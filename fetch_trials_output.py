3cc56f552242b1f21780de14916a99fa9562b44d
timmy6602066 <71965899+timmy6602066@users.noreply.github.com>
2024-12-06 20:46:00 +0100

    Update fetch_trials_output.py

--- fetch_trials_output.py
+++ fetch_trials_output.py
@@ -72,12 +72,9 @@ def process_with_groq(input_text):
             {
                 "role": "system",
                 "content": (
-                 "Please start by explaining the clinical significance of this trial. Then summarize the treatment details, precautions, and other relevant information about this condition based on the experimental report. Use simple and easy-to-understand language (for people without a medical background)." 
-                    "The simplification is both on the lexical level, but also the syntactic structure(Use simple sentences.). And bring more structures to the output."
-                    "Your output should look like:"
-                    "Clinical Significance: ....; "
-                    "Treatment Details:...."
-                    "and so on. Section by section"
+                 "Please bring more structures to the whole output and start by explaining the "clinical significance" of this trial. Then summarize the treatment details, precautions, and other relevant information about this condition based on the experimental report. Use simple and easy-to-understand language (for people without a medical background). " 
+                    "The simplification is both on the lexical level, but also the syntactic structure(Use simple sentences.). "
+                
                 ),
             },
             {
